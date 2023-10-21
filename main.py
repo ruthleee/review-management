5234da0f640ae0540e96f7c62926e55e697759d1
ruthleee <36516745+ruthleee@users.noreply.github.com>
2023-10-21 18:20:20 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+import flask
+
+# If `entrypoint` is not defined in app.yaml, App Engine will look for an app
+# called `app` in `main.py`.
+app = flask.Flask(__name__)
+
+
+@app.get("/")
+def hello():
+    """Return a friendly HTTP greeting."""
+    return "Hello Review Management!!!\n"
+
+@app.get("/review_id")
+def id():
+    """Return a friendly HTTP greeting."""
+    return "Info about a specific review here!!!\n"
+
+
+if __name__ == "__main__":
+    # Used when running locally only. When deploying to Google App
+    # Engine, a webserver process such as Gunicorn will serve the app. This
+    # can be configured by adding an `entrypoint` to app.yaml.
+    app.run(host="localhost", port=8080, debug=True)
